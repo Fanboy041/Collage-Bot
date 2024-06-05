766d0a822cde32552e1f2f9ff19e8329f0d95827
Fanboy041 <hanialhusen041@gmail.com>
2024-06-06 01:56:50 +0300

    Update lib to telethon

--- src/Commands/startCommand.py
+++ src/Commands/startCommand.py
@@ -1,27 +1,58 @@
-from Database.MongoDB import owner_collection, save_owner, user_collection, save_user, get_owner
+from Database.MongoDB import owner_collection, user_collection, save_owner, save_user, get_owner, get_admin
+from telethon.tl.types import PeerUser
 
-def start_command(message, bot):
-    if message.chat.type == "private":
 
-        first_name = message.from_user.first_name
-        last_name = message.from_user.last_name
+async def send_welcome(event, bot):
+    me = await bot.get_me()
+    sender = await event.get_sender()
+    chat_id = event.message.peer_id
+    if isinstance(chat_id, PeerUser):
+
+        # User's information
+        first_name = sender.first_name
+        last_name = sender.last_name
         if last_name:
             full_name = first_name + " " + last_name
         else:
             full_name = first_name
 
-        username = message.from_user.username
-        chat_id = message.chat.id
+        username = sender.username
 
+        chat_id = chat_id.user_id
+
+        # Set owner if it's the first user and there is one owner only
         if owner_collection.count_documents({}) == 0:
             save_owner(full_name, username, chat_id)
-            bot.send_message(message.chat.id, f"أهلا <b>{full_name}</b>\nلقد أصبحت مالك البوت من الآن فصاعدا", parse_mode='HTML')
+            await bot.send_message(chat_id, f"Welcome <b>{full_name}</b>\nYou are my owner from now on",
+                                   parse_mode='HTML')
 
         else:
+            # Counting the number of the users
             total_users = user_collection.count_documents({}) + 1
+
+            # Save the user info in the database
             save_user(full_name, username, chat_id, total_users)
 
-            if message.chat.id == get_owner()['chat_id']:
-                bot.send_message(message.chat.id, f"مرحبا أيها المالك, <b>{full_name}</b>. 🌹", parse_mode='HTML')
+            if chat_id == get_owner()['chat_id']:
+                await bot.send_message(chat_id,
+                                       f"Hey owner, <b>{full_name}</b>!\n\nThank you for interacting with me. I'm "
+                                       "excited to have you on board. 🌹",
+                                       parse_mode='HTML')
+
+            elif get_admin(chat_id) is not None and chat_id == get_admin(chat_id)['chat_id']:
+                await bot.send_message(chat_id,
+                                       f"Hey admin, <b>{full_name}</b>!\n\nThank you for interacting with me. I'm "
+                                       "excited to have you on board. 🌹",
+                                       parse_mode='HTML')
+
             else:
-                bot.send_message(message.chat.id, f"مرحبًا، <b>{full_name}</b>!\n\nشكرًا لك على التفاعل مع الروبوت الخاص بنا. نحن متحمسون لوجودك معنا. 🌹", parse_mode='HTML')
+                await bot.send_message(chat_id,
+                                       f"Welcome, <b>{full_name}</b>!\n\nThank you for interacting with our Telegram "
+                                       "bot. We're excited to have you on board. 🌹",
+                                       parse_mode='HTML')
+
+    else:
+        bot_username = me.username
+        if f"@{bot_username}" in event.text:
+            await bot.send_message(chat_id, reply_to=event.message.id, message="Please run the command in private")
+            # await bot.reply_to(event, "Please run the command in private")
--- src/main.py
+++ src/main.py
@@ -1,44 +1,53 @@
-import telebot, logging
+import logging
 import os
 import importlib
 from logging.handlers import RotatingFileHandler
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from telethon import TelegramClient, events
+
+from Commands import startCommand
+
+# load the .env file
 load_dotenv()
 
-bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+# Create the client and connect
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+bot_token = os.getenv('BOT_TOKEN')
+bot = TelegramClient('bot', int(api_id), api_hash).start(bot_token=bot_token)
+
 
 try:
+    # Logging configuration
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    max_log_size_mb = 5
+    # RotatingFileHandler
+    max_log_size_mb = 5  # Set your desired maximum log size in megabytes
     file_handler = RotatingFileHandler('./bot.log', maxBytes=max_log_size_mb * 1024 * 1024, backupCount=1)
     file_handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     logging.getLogger().addHandler(file_handler)
 
-    commands_dir = os.path.join(os.path.dirname(__file__), 'Commands')
-    commands = {}
-
-    for foldername in os.listdir(commands_dir):
-        if foldername.endswith('.py') and foldername != '__init__.py':
-
-            command_name = os.path.splitext(foldername)[0]
-            command = importlib.import_module(f'Commands.{command_name}')
-            commands[command_name] = command
-
-    logging.info("Main script runs successfully, Bot is working")
+    # commands_dir = os.path.join(os.path.dirname(__file__), 'Commands')
+    # commands = {}
+    #
+    # for folderName in os.listdir(commands_dir):
+    #     if folderName.endswith('.py') and folderName != '__init__.py':
+    #         command_name = os.path.splitext(folderName)[0]
+    #         command = importlib.import_module(f'Commands.{command_name}')
+    #         commands[command_name] = command
+    #
+    # logging.info("Main script runs successfully, Bot is working")
 
     # Start command
-    @bot.message_handler(commands=['start'])
-    def handle_start_command(message):
-        if 'startCommand' in commands:
-            commands['startCommand'].start_command(message, bot)
+    @bot.on(event=events.NewMessage(pattern='/start'))
+    async def handle_start_command(event):
+        await startCommand.send_welcome(event, bot)
+        # if 'startCommand' in commands:
+        # commands['startCommand'].send_welcome(event, bot)
 
-
-
-
-    bot.infinity_polling()
+    bot.run_until_disconnected()
 except KeyboardInterrupt:
     logging.info("Polling manually interrupted.")
